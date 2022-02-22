1178e1582023e2510647d7b9d9c212d9257d1f4c
Martin <martinmarescr@gmail.com>
2022-02-21 21:07:34 -0600

    Se crea kata 10

---
+++ Todas las Katas Resueltas/kata10/open.py
@@ -0,0 +1,13 @@
+
+try:
+     open('config.txt')
+except FileNotFoundError:
+     print("Couldn't find the config.txt file!")
+
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
